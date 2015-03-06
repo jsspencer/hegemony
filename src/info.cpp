840f88915eabae2aee5070abf423aaa3257af4e6
James Spencer <j.spencer@imperial.ac.uk>
2015-03-06 14:29:39 +0000

    Add git info to banner.

--- src/info.cpp
+++ src/info.cpp
@@ -2,12 +2,16 @@
 #include <fstream>
 #include <omp.h>
 
+#define STRINGIFY(x) #x
+#define EXPAND(x) STRINGIFY(x)
+
 void print_banner(void)
 {
     std::cout << " --- Hegemony --- " << std::endl;
     const time_t now = time((time_t*)NULL);
     std::cout << " " << ctime(&now);
     std::cout << " Compiled on " << __DATE__ << " at " << __TIME__ << std::endl;
+    std::cout << " Git SHA1: " << EXPAND(GIT_SHA1) << std::endl;
 #ifdef _OPENMP
     if (omp_get_max_threads() > 1)
         std::cout << " Running on " << omp_get_max_threads() << " threads" << std::endl;
