c4aa47860f5414ced739ceac0ca16141e3c1597d
James Spencer <j.spencer@imperial.ac.uk>
2015-03-08 22:14:16 +0000

    Overloaded + and - operators for KPoint class.

--- src/basis.h
+++ src/basis.h
@@ -24,6 +24,22 @@ class KPoint
             kin += double(i*i)/2;
     }
 
+    friend inline KPoint operator+(const KPoint &k1, const KPoint &k2)
+    {
+        KPoint ksum = KPoint();
+        for (int i=0; i<ndim; i++)
+            ksum.k[i] = k1.k[i] + k2.k[i];
+        return ksum;
+    }
+
+    friend inline KPoint operator-(const KPoint &k1, const KPoint &k2)
+    {
+        KPoint kdiff = KPoint();
+        for (int i=0; i<ndim; i++)
+            kdiff.k[i] = k1.k[i] - k2.k[i];
+        return kdiff;
+    }
+
     friend inline bool operator<(const KPoint &lhs, const KPoint &rhs)
     {
         return lhs.kin < rhs.kin;
